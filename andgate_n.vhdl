cbc3411ff2c795d95a563ca508bfe7c011dcbfac
Reza Esmaeelzade <49592379+reza-esmaeel-zade@users.noreply.github.com>
2022-11-12 13:46:22 +0330

    Add files via upload

---
+++ andgate_n.vhdl
@@ -0,0 +1,29 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity andgate_n is
+	generic(
+		n:integer
+		);
+	port (
+		a: in std_logic_vector(n-1 downto 0);
+		y: out std_logic
+		);
+end andgate_n;
+
+architecture behavioral of andgate_n is
+
+	begin
+
+		process(a)
+			variable temp:std_logic;
+
+			begin
+				temp:='1';
+				for i in n-1 downto 0 loop
+					temp:=temp and a(i);
+
+				end loop;
+			y<=temp;
+		end process;
+
+end behavioral;
